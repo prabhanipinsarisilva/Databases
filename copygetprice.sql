8377d35cacd084af4e22333a4e5d8fece567eb47
prabhanipinsarisilva <65328405+prabhanipinsarisilva@users.noreply.github.com>
2021-03-04 01:38:14 +0530

    Add files via upload

---
+++ copygetprice.sql
@@ -0,0 +1,32 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Scalar Function (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the function.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date, ,>
+-- Description:	<Description, ,>
+-- =============================================
+CREATE FUNCTION dbo.ufnCopyGetPrice(@copy_NO varchar)
+
+
+returns money
+as
+begin
+declare @Price money 
+Select @Price =Price from copy
+Where  @copy_NO = @copy_NO
+return @price
+end
+
