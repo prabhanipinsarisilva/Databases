980a48a45b3a8c5230236103860555d53cf7bb23
prabhanipinsarisilva <65328405+prabhanipinsarisilva@users.noreply.github.com>
2021-03-04 01:33:46 +0530

    Add files via upload

---
+++ Borrower_insert trigger.sql
@@ -0,0 +1,34 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Trigger (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- See additional Create Trigger templates for more
+-- examples of different Trigger statements.
+--
+-- This block of comments will not be included in
+-- the definition of the function.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE TRIGGER Borrower_insert ON Borrower
+   AFTER INSERT
+AS 
+BEGIN
+	
+	SELECT*FROM inserted;
+
+ 
+
+END
+GO
