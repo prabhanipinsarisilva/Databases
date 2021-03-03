a21ba6b025b2b75a5450f1b4133a779251020e66
prabhanipinsarisilva <65328405+prabhanipinsarisilva@users.noreply.github.com>
2021-03-04 01:34:41 +0530

    Add files via upload

---
+++ copy.sql
@@ -0,0 +1,43 @@
+use Library_System;
+CREATE table copy(
+copy_NO varchar (20) NOT nULL,
+Price Money ,
+
+CONSTRAINT pk_copyno PRIMARY KEY (copy_NO),
+);
+INSERT INTO  copy (copy_NO ,Price)
+
+values ('1','100') ,
+       ('2','250') ,
+       ('3','300') ,
+       ('4','350') ,
+       ('5','450') ,
+	   ('6','500') ,
+	   ('7','550') ,
+	   ('8','600') ,
+	   ('9','650') ,
+	   ('10','700');
+ 
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
